bd59a3d9d3eca8e761a5a191cefebba655a8822b
Andrew Waller <andrewhwaller@gmail.com>
2019-03-16 00:37:25 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turn_count = 0
   board.each do |spots_taken|
-    position_taken?
+    if spots_taken == !(" " || "" || nil)
+    turn_count += 1  
   end
-  turn_count += 1
 end
